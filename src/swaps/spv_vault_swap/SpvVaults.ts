a64e1a41d693d6bd2691b1fd2397db5bae2232a9
adambor <adamborteam@gmail.com>
2025-03-21 11:32:59 +0100

    Also set blockhash for confirmed txs

--- src/swaps/spv_vault_swap/SpvVaults.ts
+++ src/swaps/spv_vault_swap/SpvVaults.ts
@@ -289,8 +289,12 @@ export class SpvVaults {
                             changed = true;
                         } else {
                             //Update confirmations count
-                            if(pendingWithdrawal.btcTx.confirmations !== btcTx.confirmations) {
+                            if(
+                                pendingWithdrawal.btcTx.confirmations !== btcTx.confirmations ||
+                                pendingWithdrawal.btcTx.blockhash !== btcTx.blockhash
+                            ) {
                                 pendingWithdrawal.btcTx.confirmations = btcTx.confirmations;
+                                pendingWithdrawal.btcTx.blockhash = btcTx.blockhash;
                                 changed = true;
                             }
                         }
