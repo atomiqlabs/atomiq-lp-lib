e2ccad24c4df2d12467acec279c7f0beaaf92a71
adambor <adamborteam@gmail.com>
2025-07-04 16:53:21 +0200

    Fix flipped condition

--- dist/swaps/spv_vault_swap/SpvVaultSwapHandler.js
+++ dist/swaps/spv_vault_swap/SpvVaultSwapHandler.js
@@ -378,7 +378,7 @@ class SpvVaultSwapHandler extends SwapHandler_1.SwapHandler {
                     msg: "PSBT transaction cannot be parsed!"
                 };
             }
-            if (data.isRecipient(swap.recipient) ||
+            if (!data.isRecipient(swap.recipient) ||
                 data.callerFeeRate !== swap.callerFeeShare ||
                 data.frontingFeeRate !== swap.frontingFeeShare ||
                 data.executionFeeRate !== swap.executionFeeShare ||
