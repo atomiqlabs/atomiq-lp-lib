3ec7ea5869012b14f99afb6a308b2df5de4eb7fd
adambor <adamborteam@gmail.com>
2026-08-06 11:04:28 +0200

    Change order of execution of async and sync code in spv from btc swap handlers

--- dist/swaps/spv_vault_swap/SpvVaultSwapHandler.js
+++ dist/swaps/spv_vault_swap/SpvVaultSwapHandler.js
@@ -726,11 +726,11 @@ class SpvVaultSwapHandler extends SwapHandler_1.SwapHandler {
                 swap.state = SpvVaultSwap_1.SpvVaultSwapState.SIGNED;
                 swap.sending = true;
                 swapSendingSet = true;
-                await this.saveSwapData(swap);
                 data.btcTx.raw = btcRawTx;
                 data.sending = true;
                 vault.addWithdrawal(data);
                 dataSendingSet = true;
+                await this.saveSwapData(swap);
                 await this.Vaults.saveVault(vault);
                 this.swapLogger.info(swap, "REST: /postQuote: BTC transaction signed, txId: " + txId);
                 try {
